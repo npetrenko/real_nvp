6bec53384db18d396594a3b89794cb94340a2944
Nikita Petrenko <npetrenko97@gmail.com>
2018-03-20 15:23:57 +0300

    normal redistr

--- flows/flows/basedistr.py
+++ flows/flows/basedistr.py
@@ -15,10 +15,19 @@ class Normal(Distribution):
         self.mu = float(mu)
         self.dim = dim
 
-    def logdens(self, x):
+    def logdens(self, x, mean=False, full_reduce=True):
         s2 = tf.square(self.sigma)
         tmp = -tf.square(x-self.mu)/(2*s2) - 0.5 * tf.log(2*np.pi*s2)
-        return tf.reduce_sum(tmp)
+
+        if full_reduce:
+            idx = list(range(len(tmp.shape)))
+        else:
+            idx = [i for i in range(len(tmp.shape)) if i > 0]
+
+        if mean:
+            return tf.reduce_mean(tmp, idx)
+        else:
+            return tf.reduce_sum(tmp, idx)
 
     def sample(self):
         return tf.random_normal([self.dim], self.mu, self.sigma)
@@ -28,10 +37,10 @@ class NormalRW(Normal):
         super().__init__(dim, mu, sigma)
         self.init_distr = Normal(None, mu0, sigma0)
 
-    def logdens(self, x):
+    def logdens(self, x, mean=False, full_reduce=True):
         assert len(x.shape) >= 2
         norms = x[:,1:] - x[:,:-1]
-        ll = super().logdens(norms) + self.init_distr.logdens(x[:,0])
+        ll = super().logdens(norms, mean, full_reduce) + self.init_distr.logdens(x[:,0:1], mean, full_reduce)
         return ll
     
     def sample(self):
--- flows/flows/debug.py
+++ flows/flows/debug.py
@@ -1,3 +1,13 @@
+from __future__ import print_function
+import tensorflow as tf
 from tensorflow.python import debug as tf_dbg
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 wrapper = tf_dbg.LocalCLIDebugWrapperSession
+
+def write_graph():
+    writer = tf.summary.FileWriter('/tmp/tfdbg')
+    writer.add_graph(tf.get_default_graph())
