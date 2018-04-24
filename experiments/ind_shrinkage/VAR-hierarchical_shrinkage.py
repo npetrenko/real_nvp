corrected rsquared calculation, added shrinkage experiments

experiments/ind_shrinkage/VAR-hierarchical_shrinkage.py
@@ -58,24 +58,26 @@ tf.add_to_collection('logdensities', global_inf.logdens[0])
 
 
 
-with tf.variable_scope('variation_rate', dtype=floatX):
-    variation_prior = tf.distributions.Exponential(rate=.3)
-    dim_ = (VAR_DIM*2+1)*VAR_DIM
-    variation_mu = tf.get_variable('mu',trainable=False, shape=[dim_], initializer=tf.constant_initializer(math.log(0.3)))
-    variation_presigma = tf.get_variable('presigma', trainable=True, shape=[dim_], initializer=tf.constant_initializer(-3.))
-    variation_d = LogNormal(dim=dim_, mu=variation_mu, sigma=tf.exp(variation_presigma))
-    
-    variation = variation_d.sample()
+def get_ind_variation():
+    with tf.variable_scope('variation_rate', dtype=floatX):
+        variation_prior = tf.distributions.Exponential(rate=.3)
+        dim_ = (VAR_DIM*2+1)*VAR_DIM
+        variation_mu = tf.get_variable('mu',trainable=False, shape=[dim_], initializer=tf.constant_initializer(math.log(0.3)))
+        variation_presigma = tf.get_variable('presigma', trainable=True, shape=[dim_], initializer=tf.constant_initializer(-3.))
+        variation_d = LogNormal(dim=dim_, mu=variation_mu, sigma=tf.exp(variation_presigma))
+        
+        variation = variation_d.sample()
 
-    pp = tf.cast(tf.reduce_sum(variation_prior.log_prob(tf.cast(variation, tf.float32))), floatX)
-    ld = variation_d.logdens(variation)
-    tf.add_to_collection('logdensities', ld)
-    tf.add_to_collection('priors', pp)
+        pp = tf.cast(tf.reduce_sum(variation_prior.log_prob(tf.cast(variation, tf.float32))), floatX)
+        ld = variation_d.logdens(variation)
+        tf.add_to_collection('logdensities', ld)
+        tf.add_to_collection('priors', pp)
 
-    tf.summary.histogram('variation', variation)
-    tf.summary.scalar('mean_variation', tf.reduce_mean(variation))
+        tf.summary.histogram('variation', variation)
+        tf.summary.scalar('mean_variation', tf.reduce_mean(variation))
 
-individ_variation_prior = Normal((VAR_DIM*2+1)*VAR_DIM, sigma=variation, mu=global_inf.output[0])
+    return variation
+
 
 models = []
 indiv_logdens = []
@@ -85,6 +87,9 @@ indivs = {}
 with tf.variable_scope(tf.get_variable_scope(), dtype=floatX, reuse=tf.AUTO_REUSE):
     for country, data in country_data.items():
         with tf.variable_scope(country):
+            variation = get_ind_variation()
+            individ_variation_prior = Normal((VAR_DIM*2+1)*VAR_DIM, sigma=variation, mu=global_inf.output[0])
+
             individ_variation = DFlow([NVPFlow((VAR_DIM*2+1)*VAR_DIM, 
                                                name='nvp_{}'.format(i), 
                                                aux_vars=global_inf.output) for i in range(6)], init_sigma=0.01)
@@ -112,14 +117,14 @@ kls = tf.summary.scalar('KLd', kl)
 summary = tf.summary.merge_all()
 
 
-main_op = tf.train.AdamOptimizer(0.0001).minimize(kl)
+main_op = tf.train.AdamOptimizer(0.001).minimize(kl)
 
 sess = tf.InteractiveSession()
 init = tf.global_variables_initializer()
 
 init.run()
 
-writer = tf.summary.FileWriter('/home/nikita/tmp/tblogs/custom_gvar_exp_variation_rate0.3')
+writer = tf.summary.FileWriter('/home/nikita/tmp/tblogs/custom_gvar_exp_variation_rate0.3_ind_variation')
 
 def validate_year(year):
     cdic = {model.name:model for model in models}
@@ -147,7 +152,7 @@ def validate_year(year):
 
 saver = tf.train.Saver()
 
-for epoch in tqdm(range(1000)):
+for epoch in tqdm(range(200)):
     fd = {current_year:YEARS[0]}
     for step in range(100):
         sess.run(main_op, fd)
@@ -157,7 +162,7 @@ for epoch in tqdm(range(1000)):
 validations = []
 for year in tqdm(YEARS):
     fd = {current_year: year}
-    for epoch in range(epoch, epoch+80//4):
+    for epoch in range(epoch, epoch+8):
         for step in range(100):
             sess.run(main_op, fd)
         s, _ = sess.run([summary, main_op], fd)

experiments/shrinkage_cond/VAR-hierarchical_shrinkage.py
@@ -50,19 +50,11 @@ country_data = {c:d for c,d in zip(ccodes, datas)}
 current_year = tf.placeholder(tf.float32, shape=(), name='current_year')
 tf.summary.scalar('current_year', current_year)
 
-
-global_inf = DFlow([NVPFlow(dim=(VAR_DIM*2+1)*VAR_DIM, name='flow_{}'.format(i)) for i in range(6)], init_sigma=0.08)
-global_prior = Normal(None, sigma=.3).logdens(global_inf.output)
-tf.add_to_collection('priors', global_prior)
-tf.add_to_collection('logdensities', global_inf.logdens[0])
-
-
-
 with tf.variable_scope('variation_rate', dtype=floatX):
     variation_prior = tf.distributions.Exponential(rate=.3)
     dim_ = (VAR_DIM*2+1)*VAR_DIM
-    variation_mu = tf.get_variable('mu',trainable=False, shape=[dim_], initializer=tf.constant_initializer(math.log(0.3)))
-    variation_presigma = tf.get_variable('presigma', trainable=True, shape=[dim_], initializer=tf.constant_initializer(-3.))
+    variation_mu = tf.get_variable('mu', shape=[dim_], initializer=tf.constant_initializer(math.log(0.3)))
+    variation_presigma = tf.get_variable('presigma', shape=[dim_], initializer=tf.constant_initializer(-3.))
     variation_d = LogNormal(dim=dim_, mu=variation_mu, sigma=tf.exp(variation_presigma))
     
     variation = variation_d.sample()
@@ -75,6 +67,11 @@ with tf.variable_scope('variation_rate', dtype=floatX):
     tf.summary.histogram('variation', variation)
     tf.summary.scalar('mean_variation', tf.reduce_mean(variation))
 
+global_inf = DFlow([NVPFlow(dim=(VAR_DIM*2+1)*VAR_DIM, name='flow_{}'.format(i), aux_vars=variation[tf.newaxis]) for i in range(6)], init_sigma=0.01)
+global_prior = Normal(None, sigma=1.).logdens(global_inf.output)
+tf.add_to_collection('priors', global_prior)
+tf.add_to_collection('logdensities', global_inf.logdens[0])
+
 individ_variation_prior = Normal((VAR_DIM*2+1)*VAR_DIM, sigma=variation, mu=global_inf.output[0])
 
 models = []
@@ -85,11 +82,12 @@ indivs = {}
 with tf.variable_scope(tf.get_variable_scope(), dtype=floatX, reuse=tf.AUTO_REUSE):
     for country, data in country_data.items():
         with tf.variable_scope(country):
+            aux = tf.concat([global_inf.output, variation[tf.newaxis]], axis=-1)
             individ_variation = DFlow([NVPFlow((VAR_DIM*2+1)*VAR_DIM, 
                                                name='nvp_{}'.format(i), 
-                                               aux_vars=global_inf.output) for i in range(6)], init_sigma=0.01)
+                                               aux_vars=aux) for i in range(6)], init_sigma=0.01)
 
-            ind = individ_variation.output[0]
+            ind = individ_variation.output[0] + global_inf.output[0]
             indivs[country] = ind
 
             indiv_logdens.append(individ_variation.logdens)
@@ -119,7 +117,7 @@ init = tf.global_variables_initializer()
 
 init.run()
 
-writer = tf.summary.FileWriter('/home/nikita/tmp/tblogs/custom_gvar_exp_variation_rate0.3')
+writer = tf.summary.FileWriter('/home/nikita/tmp/tblogs/custom_gvar_indiv_var_fullcond')
 
 def validate_year(year):
     cdic = {model.name:model for model in models}
@@ -164,6 +162,6 @@ for year in tqdm(YEARS):
         writer.add_summary(s, global_step=epoch)
     validations.append(validate_year(year))
 
-    saver.save(sess, './save/custom_gvar_exp_variation_rate0.3')
-    with open('output_evar_rate0.3.pkl', 'wb') as f:
+    saver.save(sess, './save/custom_gvar_fullcond')
+    with open('output_evar_fullcond.pkl', 'wb') as f:
         pkl.dump(validations,f)

flows/flows/models.py
@@ -98,7 +98,8 @@ class VARmodel:
 
         sigmas = tf.diag_part(self.obs_d.sigma)
 
-        std = tf.nn.moments(self.data[0,1:,:self.var_dim] - self.data[0,:-1,:self.var_dim], axes=[0])[1]
+        current_data = self.data[:,:self.OBSERV_STEPS]
+        std = tf.nn.moments(current_data[0,1:,:self.var_dim] - current_data[0,:-1,:self.var_dim], axes=[0])[1]
         print(std, sigmas)
         rsquareds = 1 - sigmas/std
         self.create_summary(tf.summary.scalar, 'rsquared_post_mean', tf.reduce_mean(rsquareds))